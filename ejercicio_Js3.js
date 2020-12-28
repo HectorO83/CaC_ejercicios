bc745eee7f6ce54a484f4a7a8b4d26050573a666
HectorO83 <hectororbegoso83@gmail.com>
2020-12-27 22:49:41 -0300

    added index.css and added function in .js for the first step in creating dark mode

--- ejercicio_Js3.js
+++ ejercicio_Js3.js
@@ -35,3 +35,11 @@ function validateEmail(e) {
         }
     }
 }
+
+
+const switchButton = document.getElementById('switch');
+
+switchButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
+    switchButton.classList.toggle('active'); //toggle the HTML button with the id='switch' with the class 'active'
+});
