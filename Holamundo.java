ac9599f4afff82435a7edf697d014f98a181cff3
HectorO83 <hectororbegoso83@gmail.com>
2020-12-27 21:59:27 -0300

    ejercicio java 3

---
+++ Holamundo.java
@@ -0,0 +1,13 @@
+public class Holamundo{
+
+	public static void main(String[] args){
+
+		String name = "Hector";
+		Integer age = 99;
+
+		System.out.println("Hola mundo antiguo");
+		System.out.println("¡Hola, " + name + "! Tenés " + age + " anyos");
+
+	}
+
+}
